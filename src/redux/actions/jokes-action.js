17a6d3d1ba854c86c952ff2d5f5a9074bd37602b
Jayesh Tiwari <JayeshTiwari03@users.noreply.github.com>
2022-08-26 12:24:29 +0530

    Code Refactor

--- src/redux/actions/jokes-action.js
+++ src/redux/actions/jokes-action.js
@@ -3,7 +3,5 @@ export const FETCH_JOKES_SUCCESS = "FETCH_JOKES_SUCCESS";
 export const FETCH_JOKES_FAILURE = "FETCH_JOKES_FAILURE";
 
 export function getData(inputValue) {
-  console.log(inputValue, "inside getdata function");
-  console.log("called");
   return { type: "FETCH_JOKES_REQUEST", payload: { inputValue } };
-}
+};
