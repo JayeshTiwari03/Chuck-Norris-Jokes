b144060c9e86b9d559b73cb4571c01422cdc7641
Jayesh Tiwari <JayeshTiwari03@users.noreply.github.com>
2022-08-26 12:27:44 +0530

    Update JokesList.js

--- src/components/JokesList.js
+++ src/components/JokesList.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { connect } from "react-redux";
-const JokesList = (props) => {
+
+const JokesList = props => {
   const jokes = props.jokes && props.jokes.value;
-  //console.log(props.jokes.value, "list");
+
   return (
     <>
       {jokes == null
@@ -13,14 +14,16 @@ const JokesList = (props) => {
                 <div className="content">{i.joke}</div>
               </div>
             </div>
-          ))}
+          ))
+      }
     </>
   );
 };
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     jokes: state.jokes,
   };
 };
+
 export default connect(mapStateToProps)(JokesList);
